753fb9a687ed3a71825aec1fb579c3026de97d9b
alexglow <alex.glowaski@gmail.com>
2020-11-06 11:28:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var udpPort = new osc.UDPPort({
 
 board.on("ready", function() {
 
-  var led = new five.Led(4);
+  var led = new five.Led(13); // usual built-in LED pin on arduinos
 
   udpPort.open();
 
